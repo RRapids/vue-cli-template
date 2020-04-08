d76593fd7e03db01f630adbc320ab918d354b479
RRapids <tengfeiqaq@gmail.com>
2020-04-08 10:56:59 +0800

    vue模板

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Card from '../views/Card.vue'
+import List from '../views/List.vue'
+import Home from '../views/Home.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: 'home'
+  },
+  {
+    path: '/welcome',
+    name: 'Welcome',
+    component: () => import('@/views/Welcome.vue')
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    children: [
+      {
+        path: '/',
+        redirect: 'card'
+      },
+      {
+        path: 'card',
+        name: 'Card',
+        component: Card
+      },
+      {
+        path: 'list',
+        name: 'List',
+        component: List
+      }
+    ]
+  },
+  {
+    name: '404',
+    path: '*',
+    component: () => import('@/views/404.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>列表视图</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data() {
+    return {}
+  },
+  components: {},
+  created() {},
+  mounted() {},
+  methods: {},
+  computed: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
